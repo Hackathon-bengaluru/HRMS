4000e46b0c775a15aadee51407e29f80248fbd74
Bethapudi Bhaskar Siva Sai Prasad <bb495157@gmail.com>
2026-08-22 10:45:00 +0530

    Add HRMS project structure and login frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/auth/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* Redirect root to login for now */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ server/src/app.js
@@ -0,0 +1,7 @@
+// Express application setup
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+export default app;
